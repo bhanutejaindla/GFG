d8e367f206d39dcce9b6b25b8a2badd760b1d74e
bhanutejaindla <87014662+bhanutejaindla@users.noreply.github.com>
2024-10-20 15:52:31 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Minimum Platforms/minimum-platforms.cpp
@@ -0,0 +1,60 @@
+//{ Driver Code Starts
+// Program to find minimum number of platforms
+// required on a railway station
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+    // Function to find the minimum number of platforms required at the
+    // railway station such that no train waits.
+    int findPlatform(vector<int>& arr, vector<int>& dep) {
+        // Your code here
+        sort(arr.begin(),arr.end());
+        sort(dep.begin(),dep.end());
+        int a=1;
+        int d=0;
+        int plat=1;
+        int maxplat=1;
+        while(a<arr.size() && d<dep.size())
+        {
+            if(arr[a]<=dep[d])
+            {
+                plat++;
+                a++;
+                maxplat=max(maxplat,plat);
+            }
+            else
+            {
+                 plat--;
+                 d++;
+            }
+        }
+        return maxplat;
+    }
+};
+
+
+//{ Driver Code Starts.
+//  Driver code
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        vector<int> arr(n);
+        vector<int> dep(n);
+        for (int i = 0; i < n; i++)
+            cin >> arr[i];
+        for (int j = 0; j < n; j++) {
+            cin >> dep[j];
+        }
+        Solution ob;
+        cout << ob.findPlatform(arr, dep) << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
