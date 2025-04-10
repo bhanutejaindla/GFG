1f3d11fb9f114a28abb8e6a75e8016b05d6d79b2
bhanutejaindla <87014662+bhanutejaindla@users.noreply.github.com>
2025-04-10 16:30:45 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Longest Increasing Subsequence/longest-increasing-subsequence.cpp
@@ -0,0 +1,60 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+class Solution {
+  public:
+    int lis(vector<int>& arr) {
+        // code here
+        vector<int>temp;
+        temp.push_back(arr[0]);
+        int len=1;
+        for(int i=1;i<arr.size();i++)
+        {
+            if(arr[i]>temp.back())
+            {
+                temp.push_back(arr[i]);
+                len++;
+            }
+            else
+            {
+                int ind=lower_bound(temp.begin(),temp.end(),arr[i])-temp.begin();
+                temp[ind]=arr[i];
+            }
+        }
+        return len;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore(); // to ignore the newline after the integer input
+
+    while (t--) {
+        int n;
+        vector<int> arr;
+        string input;
+
+        // Input format: first number n followed by the array elements
+        getline(cin, input);
+        stringstream ss(input);
+        int num;
+        while (ss >> num)
+            arr.push_back(num);
+
+        Solution obj;
+        cout << obj.lis(arr) << endl;
+        cout << "~" << endl;
+    }
+
+    return 0;
+}
+
+// } Driver Code Ends
